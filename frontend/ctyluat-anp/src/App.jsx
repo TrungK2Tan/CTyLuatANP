d9761cb83f6f7262c941189a82aad2c559ada954
trinhtuandan03 <trinhtuandan1601@gmail.com>
2025-05-03 01:23:26 +0700

    UpdateUpdate Number Page

--- frontend/ctyluat-anp/src/App.jsx
+++ frontend/ctyluat-anp/src/App.jsx
@@ -6,6 +6,7 @@ import {
   Navigate,
   useLocation,
 } from "react-router-dom";
+// Import các trang user
 import Home from "./pages/Home/Home";
 import Company from "./pages/Introduce/Company";
 import FloatingIcons from "./components/FloatingIcons";
@@ -13,9 +14,19 @@ import Members from "./pages/Introduce/Members";
 import Form from "./pages/Form/Form";
 import News from "./pages/News/News";
 import Contact from "./pages/Contact/Contact";
-import HomeAdmin from "./pages/Admin/HomeAdmin";
-import LoginAdmin from "./pages/Admin/LoginAdmin";
 import FormDetail from "./pages/Form/FormDetail";
+import NewsDetail from "./pages/News/NewsDetail";
+import Services from "./pages/Services/Services";
+import ServiceDetail from "./pages/Services/ServicesDetail";
+import LegalConsultPopup from "./popup/LegalConsultPopup";
+// Import các trang admin
+import NewsManagementPost from "./pages/Admin/ManagementPost/NewsManagementPost";
+import CreatePost from "./pages/Admin/ManagementPost/CreatePost";
+import DetailPost from "./pages/Admin/ManagementPost/DetailPost";
+import EditPost from "./pages/Admin/ManagementPost/EditPost";
+import NewsDetailManagement from "./pages/Admin/ManagementNews/NewsDetailManagement";
+import EditNews from "./pages/Admin/ManagementNews/EditNews";
+import CreateNews from "./pages/Admin/ManagementNews/CreateNews";
 import CreateForm from "./pages/Admin/Forms/CreateForm";
 import FormManagement from "./pages/Admin/Forms/FormManagement";
 import NewsManagement from "./pages/Admin/ManagementNews/NewsManagement";
@@ -23,17 +34,8 @@ import ServicesManagement from "./pages/Admin/ManagementServices/ServicesManagem
 import FormDetailManagement from "./pages/Admin/Forms/FormDetailManagement";
 import EditForm from "./pages/Admin/Forms/EditForm";
 import EditProfile from "./pages/Admin/ProfileAdmin/EditProfile";
-import NewsDetail from "./pages/News/NewsDetail";
-import NewsDetailManagement from "./pages/Admin/ManagementNews/NewsDetailManagement";
-import EditNews from "./pages/Admin/ManagementNews/EditNews";
-import CreateNews from "./pages/Admin/ManagementNews/CreateNews";
-import Services from "./pages/Services/Services";
-import ServiceDetail from "./pages/Services/ServicesDetail";
-import NewsManagementPost from "./pages/Admin/ManagementPost/NewsManagementPost";
-import CreatePost from "./pages/Admin/ManagementPost/CreatePost";
-import DetailPost from "./pages/Admin/ManagementPost/DetailPost";
-import EditPost from "./pages/Admin/ManagementPost/EditPost";
-import LegalConsultPopup from "./popup/LegalConsultPopup";
+import HomeAdmin from "./pages/Admin/HomeAdmin";
+import LoginAdmin from "./pages/Admin/LoginAdmin";
 
 const PrivateRoute = ({ element }) => {
   const token = localStorage.getItem("token");
@@ -46,8 +48,8 @@ const AppContent = () => {
 
   return (
     <>
-    {/* Hiển thị popup khi load trang nếu không phải trang admin */}
-    {!isAdminRoute && <LegalConsultPopup />}
+      {/* Hiển thị popup khi load trang nếu không phải trang admin */}
+      {!isAdminRoute && <LegalConsultPopup />}
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/danhmuc/ve-chung-toi" exact element={<Company />} />
@@ -65,7 +67,7 @@ const AppContent = () => {
           path="/admin"
           element={<PrivateRoute exact element={<HomeAdmin />} />}
         />
-          <Route
+        <Route
           path="/admin/chinh-sua-thong-tin"
           element={<PrivateRoute exact element={<EditProfile />} />}
         />
@@ -91,15 +93,15 @@ const AppContent = () => {
           path="/admin/quan-ly-tin-tuc"
           element={<PrivateRoute exact element={<NewsManagement />} />}
         />
-           <Route
+        <Route
           path="/admin/quan-ly-tin-tuc/:slug"
           element={<PrivateRoute exact element={<NewsDetailManagement />} />}
         />
-           <Route
+        <Route
           path="/admin/quan-ly-tin-tuc/sua/:slug"
           element={<PrivateRoute exact element={<EditNews />} />}
         />
-           <Route
+        <Route
           path="/admin/tao-tin-tuc"
           element={<PrivateRoute exact element={<CreateNews />} />}
         />
@@ -108,23 +110,23 @@ const AppContent = () => {
           path="/admin/quan-ly-dich-vu-hoi-dap"
           element={<PrivateRoute exact element={<ServicesManagement />} />}
         />
-         {/* Quản lý Bài Viết  */}
-         <Route
+        {/* Quản lý Bài Viết  */}
+        <Route
           path="/admin/quan-ly-bai-viet"
           element={<PrivateRoute exact element={<NewsManagementPost />} />}
         />
-          <Route
-                  // chi tiết bài viết
+        <Route
+          // chi tiết bài viết
           path="/admin/quan-ly-bai-viet/:slug"
           element={<PrivateRoute exact element={<DetailPost />} />}
         />
         <Route
-                 // chỉnh sửa bài viết
+          // chỉnh sửa bài viết
           path="/admin/quan-ly-bai-viet/sua/:slug"
           element={<PrivateRoute exact element={<EditPost />} />}
         />
         <Route
-                  // tạo bài viết
+          // tạo bài viết
           path="/admin/tao-bai-viet"
           element={<PrivateRoute exact element={<CreatePost />} />}
         />
